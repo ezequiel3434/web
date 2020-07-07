75ecaac262b49a878ca063f7616fd9d1d0204294
Ezequiel <ezequiel3434@gmail.com>
2020-07-07 07:50:42 -0300

    Initial Commit

---
+++ web/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  web
+//
+//  Created by Ezequiel Parada Beltran on 07/07/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

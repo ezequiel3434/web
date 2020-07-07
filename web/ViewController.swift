1372c89f93fc5da944a7c40fbeb108295a97a092
Ezequiel <ezequiel3434@gmail.com>
2020-07-07 10:11:48 -0300

    Creating UI

--- web/ViewController.swift
+++ web/ViewController.swift
@@ -12,16 +12,44 @@ import WebKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var textField: UITextField!
+    var palabra: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let url = URL(string: "https://www.youtube.com")
         
-        webView.load(URLRequest(url: url!))
+        let peticion = URLRequest(url: url!)
+        let tarea = URLSession.shared.dataTask(with: peticion) { (data, response, error) in
+            
+            if error == nil {
+                let datosString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+                
+                
+                DispatchQueue.main.sync(execute: {
+                    print(datosString!)
+                })
+            } else {
+                print(error!)
+            }
+        }
+        
+        tarea.resume()
+        
+    }
+    
+    @IBAction func search(_ sender: Any) {
+        
+        if textField.text!.isEmpty {
+            
+        }else{
+            palabra = textField.text!
+        }
         
         
     }
+    
 
 
 }
